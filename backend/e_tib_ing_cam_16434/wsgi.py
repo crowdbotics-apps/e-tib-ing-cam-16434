f862fdca896ff6d93e2ef36e1cd43a3e302c8222
Crowdbotics <team@crowdbotics.com>
2020-04-30 05:33:27 +0000

    Initial commit.

---
+++ backend/e_tib_ing_cam_16434/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for e_tib_ing_cam_16434 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_tib_ing_cam_16434.settings')
+
+application = get_wsgi_application()
